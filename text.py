9c9bdda6e40c1b322fec98230e9cdfe4d1aef449
rubidium <rubidium@openttd.org>
2013-09-13 19:06:19 +0000

    (svn r25764) Fix-ish: copyright headers should also be added to the individual files

--- text.py
+++ text.py
@@ -1,3 +1,11 @@
+# $Id$
+#
+# This file is part of musa.
+# musa is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, version 2.
+# musa is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# See the GNU General Public License for more details. You should have received a copy of the GNU General Public License along with musa. If not, see <http://www.gnu.org/licenses/>.
+#
+
 from __future__ import with_statement
 from exception import MusaException
 import re
